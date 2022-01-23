3d507cc76255fbda3b0547ecba861d27f944cdba
tahoor <tahooryaseen541@gmail.com>
2022-01-23 11:43:34 +0530

    initial commit

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'tommie.schamberger92@ethereal.email',
+    pass: '3FzkhF7Ut17qFdx3Qx',
+  },
+   tls:{
+        rejectUnauthorized:false
+    },
+};
